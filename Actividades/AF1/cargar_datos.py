547d997089bc4b1ceed99082a37a45811bc72d12
Jaime <jaime.perez@uc.cl>
2021-09-02 14:39:44 -0400

    Implementado cargar_mascotas

--- Actividades/AF1/cargar_datos.py
+++ Actividades/AF1/cargar_datos.py
@@ -2,5 +2,15 @@ from mascota import Perro, Gato, Conejo
 
 
 def cargar_mascotas(archivo_mascotas):
-    # COMPLETAR
-    pass
+    lista_mascotas = []
+    file = open(archivo_mascotas, "r")
+    
+    file.readline()
+    for line in file:
+        mascota = line.strip().split(",")
+        mascota[4] = int(mascota[4])
+        mascota[5] = int(mascota[5])
+        lista_mascotas.append(mascota)
+
+    file.close()
+    return lista_mascotas
