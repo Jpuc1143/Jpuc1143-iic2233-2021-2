d8b0b48e4824f011e2cf82c812b6757b869e19d9
Jaime <jaime.perez@uc.cl>
2021-09-02 20:58:11 -0400

    Implementado la escritura a los csv

--- Tareas/T0/csvUtils.py
+++ Tareas/T0/csvUtils.py
@@ -7,6 +7,20 @@ def read_csv(path, max_split):
 
     return data
 
-def write_csv(path):
-    # TODO: implementar
-    pass
+def write_csv(path, data):
+    file = open(path, "r")    
+    header = file.readline()
+    file.close()
+
+    file = open(path, "w")
+    file.write(header)
+
+    if type(data) is dict:
+        for key in data:
+            for comment in data[key]:
+                file.write(",".join(comment) + "\n")
+    else:
+        for line in data:
+            file.write(",".join(line) + "\n")
+
+    file.close()
