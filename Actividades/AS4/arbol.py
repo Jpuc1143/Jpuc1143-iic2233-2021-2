ef69512db4173169fd50e2e871dc2276d77f9ffd
Jaime <jaime.perez@uc.cl>
2021-11-25 15:17:52 -0300

    Implementado Parte 2

--- Actividades/AS4/arbol.py
+++ Actividades/AS4/arbol.py
@@ -20,12 +20,37 @@ class ArbolBinario:
             self.insertar_nodo(nodo, self.raiz)
 
     def insertar_nodo(self, nuevo_nodo, padre=None):
-        # Completar
-        pass
+        if self.raiz is None:
+            self.raiz = nuevo_nodo
+        else:
+            siguiente_nodo = None
+            if nuevo_nodo.usuario.fama >= padre.usuario.fama:
+                siguiente_nodo = padre.hijo_derecho
+            else:
+                siguiente_nodo = padre.hijo_izquierdo
+
+            if siguiente_nodo is None:
+                if nuevo_nodo.usuario.fama >= padre.usuario.fama:
+                    padre.hijo_derecho = nuevo_nodo
+                else:
+                    padre.hijo_izquierdo = nuevo_nodo
+            else:
+                self.insertar_nodo(nuevo_nodo, siguiente_nodo)
 
     def buscar_nodo(self, fama, padre=None):
-        # Completar
-        pass
+        # El padre inicial no es la raiz :(
+        if padre is None:
+            padre = self.raiz
+        if padre.usuario.fama == fama:
+            return padre
+        elif fama > padre.usuario.fama:
+            siguiente = padre.hijo_derecho
+        else:
+            siguiente = padre.hijo_izquierdo
+        if siguiente is None:
+            return None
+        else:
+            return self.buscar_nodo(fama, siguiente)
 
     def print_arbol(self, nodo=None, nivel_indentacion=0):
         # No modificar
@@ -35,7 +60,7 @@ class ArbolBinario:
             self.print_arbol(self.raiz)
         else:
             print(f"{indentacion}{nodo.usuario.nombre}: "
-                  f"{nodo.usuario.correo}")
+                  f"{nodo.usuario.correo}:")
             if nodo.hijo_izquierdo:
                 self.print_arbol(nodo.hijo_izquierdo,
                                  nivel_indentacion + 1)
