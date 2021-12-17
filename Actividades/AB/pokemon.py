14816d86638e92871a72ae4ce872f09327188c7b
Jaime <jaime.perez@uc.cl>
2021-12-17 14:52:00 -0300

    Implementado Parte II

--- Actividades/AB/pokemon.py
+++ Actividades/AB/pokemon.py
@@ -3,12 +3,31 @@ import requests
 
 
 def obtener_info_habilidad(url):
-    pass
+    response = requests.get(url).json()
+    result = {
+            "name": response["name"],
+            "effect_entries": list(filter(lambda x: x["language"]["name"] == "en", response["effect_entries"]))[0]["short_effect"],
+            "pokemon": list(map(lambda x: {"name": x["pokemon"]["name"], "url": x["pokemon"]["url"]}, response["pokemon"]))
+            }
+
+    return result
 
 
 def obtener_pokemones(pokemones):
-    pass
+    result = []
+    for pokemon in pokemones:
+        response = requests.get(pokemon["url"]).json()
+        data = {
+                "id": response["id"],
+                "name": response["name"],
+                "height": response["height"],
+                "weight": response["weight"],
+                "stats": dict(map(lambda x: (x["stat"]["name"] ,{"base_stat": x["base_stat"],"effort": x["effort"]}), response["stats"])),
+                "types": list(map(lambda x: x["type"]["name"], response["types"]))
+                }
+        result.append(data)
 
+    return result
 
 def obtener_pokemon_mas_alto(pokemones):
     pass
